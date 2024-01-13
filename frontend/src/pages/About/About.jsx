a41e65d28c7c9bcff7e05d26b10ae8e288b4a7c2
Behzod <bmoydinboyev@usf.edu>
2024-01-13 11:51:40 -0500

    add about page text

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -2,9 +2,17 @@ import './about.css'
 
 const About = () => {
   return (
-    <>
+    <div className="about">
       <h1>About</h1>
-    </>
+      <div className="about-text">
+        <p>As a CS major myself, I have lurked (and still do) in many CS subreddits and discord servers.</p>
+        <p>It has been quite helpful being in a community of self-driven TC chasers and realizing that I can do it too.</p>
+        <p>Before creating this webapp, I used to check process-tracking channels and ask questions from other applicants how they performed on OAs and interviews.</p>
+        <p>It can soon be a mundane job to keep track of the minimum you gotta do to pass these stages.</p>
+        <p>So I decided to change the world and create yet another CRUD app.</p>
+      </div>
+      <div className="faq"></div>
+    </div>
   )
 }
 
