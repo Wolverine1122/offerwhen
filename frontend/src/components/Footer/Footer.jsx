dbbdffec1328c12dd08bf3b29041a1647eba1a88
Behzod <bmoydinboyev@usf.edu>
2024-01-14 11:16:47 -0500

    add footer and style highlight

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,10 +1,20 @@
+import { Link } from 'react-router-dom';
+import Logo from '../Logo/Logo';
 import './footer.css'
 
 const Footer = () => {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <div className="footer">
+      <div className="logo-brand">
+        <Logo />
+        <h3>OfferWhen</h3>
+      </div>
+      <div className="footer-links">
+        <Link to="/about">About us</Link>
+        <Link to="/contact">Contact us</Link>
+        <Link to="/privacy">Privacy policy</Link>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/Logo/logo.jsx
+++ frontend/src/components/Logo/logo.jsx
@@ -4,7 +4,7 @@ import './logo.css';
 const Logo = () => {
   return (
     <Link to="/" className="logo-container">
-      <img src="./android-chrome-512x512.png" alt="Angry Unicorn Logo" className="logo" />
+      <img src="/android-chrome-512x512.png" alt="Angry Unicorn Logo" className="logo" />
     </Link>
   );
 }
