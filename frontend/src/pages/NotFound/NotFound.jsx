711cc8b47abdea89324d1a727a3363fe760cc3b2
Behzod <bmoydinboyev@usf.edu>
2024-03-30 17:43:46 -0400

    add not foundpage

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,17 @@
 import "./notfound.css";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <>
-      <h1>NotFound</h1>
-    </>
+    <div className="not-found-page">
+      <div className="redirect-instructions">
+        <h1>Ain't no way</h1>
+        <h3>Page not found</h3>
+        <button className="action-button">
+          <Link to="/companies">Take me home</Link>
+        </button>
+      </div>
+    </div>
   );
 };
 
